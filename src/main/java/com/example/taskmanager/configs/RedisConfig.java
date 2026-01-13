a608a4d05b12f97183ba5cae565c88db91e7b45a
TheRealBlaze2004 <am9hi3ian2004@gmail.com>
2026-01-13 13:12:41 +0530

    Minor Changes to Redis Config and docker changes

--- src/main/java/com/example/taskmanager/configs/RedisConfig.java
+++ src/main/java/com/example/taskmanager/configs/RedisConfig.java
@@ -1,19 +1,19 @@
 package com.example.taskmanager.configs;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
+@ConditionalOnProperty(
+        name = "spring.cache.type",
+        havingValue = "redis"
+)
 public class RedisConfig {
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory){
